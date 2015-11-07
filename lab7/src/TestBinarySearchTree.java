96ef82f325ce7a07223dba08201e500e4360d32d
calvin <calvin@192.168.0.10>
2015-11-06 21:44:12 -0500

    commits

--- lab7/src/TestBinarySearchTree.java
+++ lab7/src/TestBinarySearchTree.java
@@ -14,40 +14,40 @@ public class TestBinarySearchTree {
       System.out.print("\nEnter an element to search: ");
       Integer key = input.nextInt();
       
-//       //complete the code as suggested in the Lab document.
-//      
-//      System.out.print("\nEnter an element to delete: "+tree.delete(key) );
-//  
-//              
-//     System.out.print("\n Insert a element: "+tree.insert(55));
-//   
-//      
-//      System.out.println("\n  Inorder:");
-//      tree.inorder();
-//      
-//      System.out.println("\n  Postorder:");
-//      tree.postorder();
-//      
-//      System.out.println("\n  Preorder:");
-//      tree.preorder();
-//      
-//      System.out.println("\n The tree path: "+ tree.path(87));
-//      
-//      System.out.println ("Enter the element to find the left subtree of: ");
-//      key = input.nextInt();
-//      System.out.print("\n List the left sub tree: ");
-//      tree.leftSubTree(67);
-//      System.out.println("");
-//      
-//      System.out.println ("Enter the element to find the right subtree of: ");
-//      key = input.nextInt();
-//      System.out.println("\n List the right sub tree:" );
-//      tree.rightSubTree(81);
-//      System.out.println("");
-//      
-//      
-//      System.out.println("\n Print the number of leaves: " + tree.getNumberOfLeaves());
-//      
+       //complete the code as suggested in the Lab document.
+      
+      System.out.print("\nEnter an element to delete: "+tree.delete(key) );
+  
+              
+     System.out.print("\n Insert a element: "+tree.insert(55));
+   
+      
+      System.out.println("\n  Inorder:");
+      tree.inorder();
+      
+      System.out.println("\n  Postorder:");
+      tree.postorder();
+      
+      System.out.println("\n  Preorder:");
+      tree.preorder();
+      
+      System.out.println("\n The tree path: "+ tree.path(87));
+      
+      System.out.println ("Enter the element to find the left subtree of: ");
+      key = input.nextInt();
+      System.out.print("\n List the left sub tree: ");
+      tree.leftSubTree(67);
+      System.out.println("");
+      
+      System.out.println ("Enter the element to find the right subtree of: ");
+      key = input.nextInt();
+      System.out.println("\n List the right sub tree:" );
+      tree.rightSubTree(81);
+      System.out.println("");
+      
+      
+      System.out.println("\n Print the number of leaves: " + tree.getNumberOfLeaves());
+      
     //  System.out.print ("Enter the element to find the predecessor: ");
     //  key = input.nextInt();
       System.out.println("\n Is the in order predecessor working: "+tree.inorderPredecessor(key));
